b67cf5895c8744f3f81f49a7b69c2984963cf490
Your Name <you@example.com>
2021-01-27 16:45:22 +0800

    完成分页功能 2021/1/27 -1

--- src/main/java/com/travle/service/QuestionService.java
+++ src/main/java/com/travle/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.travle.service;
 
+import com.travle.dto.PaginationDTO;
 import com.travle.dto.QuestionDTO;
 import com.travle.mapper.QuestionMapper;
 import com.travle.mapper.UserMapper;
@@ -20,9 +21,22 @@ public class QuestionService {
     @Autowired
     private QuestionMapper questionMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+        if (page<1){
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()){
+            page = paginationDTO.getTotalPage();
+        }
+
+        Integer offset = size * (page -1);
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -30,6 +44,8 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
     }
 }
