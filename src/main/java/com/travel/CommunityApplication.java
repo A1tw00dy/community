b416532d7e5bca78766d703a52426dc243a6c6df
Your Name <you@example.com>
2021-02-15 15:36:38 +0800

    完成mybatis generator 2021/2/15 -1

--- src/main/java/com/travel/CommunityApplication.java
+++ src/main/java/com/travel/CommunityApplication.java
@@ -1,9 +1,12 @@
 package com.travel;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.travel.mapper")
 public class CommunityApplication {
 
     public static void main(String[] args) {
